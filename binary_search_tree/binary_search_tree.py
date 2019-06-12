dd494c68093f628639d4c88cb80938170dadd993
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-12 13:02:01 -0400

    Tightened up binary_search_tree.py. Removed some unnecessary lines.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,41 +5,36 @@ class BinarySearchTree:
         self.right = None
 
     def insert(self, value):
-        while True:
-            if value < self.value:
-                if self.left == None:
-                    self.left = BinarySearchTree(value)
-                    break
-                else:
-                    return self.left.insert(value)
+        if value < self.value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
             else:
-                if self.right == None:
-                    self.right = BinarySearchTree(value)
-                    break
-                else:
-                    return self.right.insert(value)
+                self.left.insert(value)
+        else:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
     def contains(self, target):
-        while True:
-            if target == self.value:
-                return True
-            if target < self.value:
-                if self.left == None:
-                    return False
-                else:
-                    return self.left.contains(target)
-            if target > self.value:
-                if self.right == None:
-                    return False
-                else:
-                    return self.right.contains(target)
+        if target == self.value:
+            return True
+        if target < self.value:
+            if self.left == None:
+                return False
+            else:
+                return self.left.contains(target)
+        if target > self.value:
+            if self.right == None:
+                return False
+            else:
+                return self.right.contains(target)
 
     def get_max(self):
-        while True:
-            if self.right == None:
-                return self.value
-            else:
-                return self.right.get_max()
+        if self.right == None:
+            return self.value
+        else:
+            return self.right.get_max()
 
     def for_each(self, cb):
         cb(self.value)
