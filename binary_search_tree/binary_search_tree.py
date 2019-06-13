969d0fe45690dae91fbb0b66f55ba660ea4babe9
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-13 14:33:03 -0400

    Fixed commented-out code in binary_search_tree.py.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -16,29 +16,29 @@ class BinarySearchTree:
             else:
                 self.right.insert(value)
 
-    # def contains(self, target):
-    #     if target == self.value:
-    #         return True
-    #     if target < self.value:
-    #         if self.left == None:
-    #             return False
-    #         else:
-    #             return self.left.contains(target)
-    #     if target > self.value:
-    #         if self.right == None:
-    #             return False
-    #         else:
-    #             return self.right.contains(target)
+    def contains(self, target):
+        if target == self.value:
+            return True
+        if target < self.value:
+            if self.left == None:
+                return False
+            else:
+                return self.left.contains(target)
+        if target > self.value:
+            if self.right == None:
+                return False
+            else:
+                return self.right.contains(target)
 
-    # def get_max(self):
-    #     if self.right == None:
-    #         return self.value
-    #     else:
-    #         return self.right.get_max()
+    def get_max(self):
+        if self.right == None:
+            return self.value
+        else:
+            return self.right.get_max()
 
-    # def for_each(self, cb):
-    #     cb(self.value)
-    #     if self.left != None:
-    #         self.left.for_each(cb)
-    #     if self.right != None:
-    #         self.right.for_each(cb)
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left != None:
+            self.left.for_each(cb)
+        if self.right != None:
+            self.right.for_each(cb)
