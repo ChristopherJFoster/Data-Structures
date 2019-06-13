91854198ed29f42c227ad7a1d3306811b05e70c7
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-12 23:49:59 -0400

    Slightly shortened code.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -46,7 +46,7 @@ class DoublyLinkedList:
     def __init__(self, node=None):
         self.head = node
         self.tail = node
-        self.length = 1 if node is not None else 0
+        self.length = 1 if node else 0
 
     def __len__(self):
         return self.length
