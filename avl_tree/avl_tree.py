350d3e45c3e476daeb13e7791867914504eb13b2
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-13 14:31:04 -0400

    I think I've implemented insert() correctly in avl_tree.py.

--- avl_tree/avl_tree.py
+++ avl_tree/avl_tree.py
@@ -1,81 +1,107 @@
 """
-Node class to keep track of
-the data internal to individual nodes
+Node class to keep track of the data internal to individual nodes
 """
+
+
 class Node:
-  def __init__(self, key):
-    self.key = key
-    self.left = None
-    self.right = None
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
 
 """
-A tree class to keep track of things like the
-balance factor and the rebalancing logic
+A tree class to keep track of things like the balance factor and the rebalancing logic
 """
+
+
 class AVLTree:
-  def __init__(self, node=None):
-    self.node = node
-    # init height to -1 because of 0-indexing
-    self.height = -1
-    self.balance = 0
+    def __init__(self, node=None):
+        self.node = node
+        # init height to -1 because of 0-indexing
+        self.height = -1
+        self.balance = 0
 
-  """
-  Display the whole tree. Uses recursive def.
-  """
-  def display(self, level=0, pref=''):
-    self.update_height()  # Update height before balancing
-    self.update_balance()
-    
-    if self.node != None: 
-      print ('-' * level * 2, pref, self.node.key,
-        f'[{self.height}:{self.balance}]',
-        'L' if self.height == 0 else ' ')
-      if self.node.left != None:
-        self.node.left.display(level + 1, '<')
-      if self.node.right != None:
-        self.node.right.display(level + 1, '>')
+    """
+    Display the whole tree. Uses recursive def.
+    """
 
-  """
-  Computes the maximum number of levels there are
-  in the tree
-  """
-  def update_height(self):
-    pass
+    def display(self, level=0, pref=''):
+        self.update_height()  # Update height before balancing
+        self.update_balance()
 
-  """
-  Updates the balance factor on the AVLTree class
-  """
-  def update_balance(self):
-    pass
+        if self.node != None:
+            print('-' * level * 2, pref, self.node.key,
+                  f'[{self.height}:{self.balance}]',
+                  'L' if self.height == 0 else ' ')
+            if self.node.left != None:
+                self.node.left.display(level + 1, '<')
+            if self.node.right != None:
+                self.node.right.display(level + 1, '>')
 
-  """
-  Perform a left rotation, making the right child of this
-  node the parent and making the old parent the left child
-  of the new parent. 
-  """
-  def left_rotate(self):
-    pass
+    """
+    Computes the maximum number of levels there are in the tree
+    """
 
-  """
-  Perform a right rotation, making the left child of this
-  node the parent and making the old parent the right child
-  of the new parent. 
-  """
-  def right_rotate(self):
-    pass
+    def update_height(self):
+        pass
 
-  """
-  Sets in motion the rebalancing logic to ensure the
-  tree is balanced such that the balance factor is
-  1 or -1
-  """
-  def rebalance(self):
-    pass
-    
-  """
-  Uses the same insertion logic as a binary search tree
-  after the value is inserted, we need to check to see
-  if we need to rebalance
-  """
-  def insert(self, key):
-    pass
+    """
+    Updates the balance factor on the AVLTree class
+    """
+
+    def update_balance(self):
+        pass
+
+    """
+    Perform a left rotation, making the right child of this node the parent and making the old parent the left child of the new parent. 
+    """
+
+    def left_rotate(self):
+        pass
+
+    """
+    Perform a right rotation, making the left child of this node the parent and making the old parent the right child of the new parent. 
+    """
+
+    def right_rotate(self):
+        pass
+
+    """
+    Sets in motion the rebalancing logic to ensure the tree is balanced such that the balance factor is 1 or -1
+    """
+
+    def rebalance(self):
+        pass
+
+    """
+    Uses the same insertion logic as a binary search tree. After the value is inserted, we need to check to see if we need to rebalance
+    """
+
+    def insert(self, key):
+        print('self.node: ', self.node)
+        if self.node == None:
+            self.node = Node(key)
+        elif key < self.node.key:
+            if self.node.left == None:
+                self.node.left = AVLTree(Node(key))
+            else:
+                self.node.left.insert(key)
+        else:
+            print('help')
+            if self.node.right == None:
+                self.node.right = AVLTree(Node(key))
+            else:
+                self.node.right.insert(key)
+
+
+test = AVLTree()
+print('None')
+test.insert(0)
+print('0')
+test.insert(1)
+print('1')
+test.insert(2)
+print('2')
+
+print(test.display())
